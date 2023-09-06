7106bb933eb86bde818cfd5cdd348cd4f4fc8cd7
Irakli Orjonikidze <irakliorjo92@gmail.com>
2023-09-06 23:48:36 +0400

    update

--- src/app/page.js
+++ src/app/page.js
@@ -382,7 +382,7 @@ export default function Home() {
      <div className='container mx-auto'>
       <div className='grid grid-cols-2 gap-[15px] md:grid-cols-3 xl:grid-cols-4 xl:gap-[30px] py-12 '>
       {pizzas.map((pizza) => {
-        return <Pizza pizza={pizza} />
+        return <Pizza key={pizza.id} pizza={pizza} />
       })}
       </div>
      </div>
